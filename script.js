333a4a2bec34d8511242210ff1fbf35ad1e619d1
hariprasathys22 <confluenceinfotechsolutions@gmail.com>
2022-07-09 23:43:45 +0530

    Landing page design completed

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 var imgSlider = (anything) => {
-    document.querySelector('.starbucks').src = anything;
-}
+  document.querySelector(".starbucks").src = anything;
+};
 
 var changeCircleColor = (color) => {
-    const circle = document.querySelector('.circle');
-    circle.style.background = color;
-    
-}
+  const circle = document.querySelector(".circle");
+  circle.style.background = color;
+};
