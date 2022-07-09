3a4bef9f1c71fae093f7b432b27cea59c6877be4
hariprasathys22 <confluenceinfotechsolutions@gmail.com>
2022-07-10 00:02:31 +0530

    Some correction had made in js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ var changeCircleColor = (color) => {
   const circle = document.querySelector(".circle");
   circle.style.background = color;
 };
+
+var textColorChange = (colors) => {
+  const text = document.querySelector(".texts");
+  text.style.color = colors;
+};
+var buttonBackground = (colours) => {
+  const background = document.querySelector(".button");
+  background.style.background = colours;
+};
