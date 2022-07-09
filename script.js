928ebb7b87d3f04ca5e874a3327ad6b1d6905958
hariprasathys22 <confluenceinfotechsolutions@gmail.com>
2022-07-09 23:42:52 +0530

    Completed our starbucks Landing page

---
+++ script.js
@@ -0,0 +1,9 @@
+var imgSlider = (anything) => {
+    document.querySelector('.starbucks').src = anything;
+}
+
+var changeCircleColor = (color) => {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+    
+}
